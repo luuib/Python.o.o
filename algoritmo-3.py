c7ca5d2d69fa8bcf5c9ab3c76463823574a29cd6
luuib <129198298+luuib@users.noreply.github.com>
2024-07-30 11:56:46 -0300

    Add files via upload

---
+++ algoritmo-3.py
@@ -0,0 +1,10 @@
+nota = [0,2,90,30,70,60]
+soma_notas = 0
+
+for nota in nota:
+    soma_notas += nota
+print(f"A soma da notas é: {soma_notas}")
+
+notas = [10,50,70,23,40]
+soma_notas = sum(notas)
+print(f"A soma de notas é: {soma_notas}")
