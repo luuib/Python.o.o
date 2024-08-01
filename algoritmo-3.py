7409f11146b785f4d4cf5c158aaa0500736092c8
luuib <129198298+luuib@users.noreply.github.com>
2024-08-01 10:24:07 -0300

    Update algoritmo-3.py

--- algoritmo-3.py
+++ algoritmo-3.py
@@ -1,10 +1,10 @@
-nota = [0,2,90,30,70,60]
+nota = [0,2,90,30,70,60,80,75,10,60,60,90,100]
 soma_notas = 0
 
 for nota in nota:
     soma_notas += nota
 print(f"A soma da notas é: {soma_notas}")
 
-notas = [10,50,70,23,40]
+notas = [10,50,70,23,40, 48,80,0,70,88,30,100]
 soma_notas = sum(notas)
 print(f"A soma de notas é: {soma_notas}")
